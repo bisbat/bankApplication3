2d6ebeaa27a1b5bc3edf482e1002b1ef6f2ef3d6
bisbat <110973307+bisbat@users.noreply.github.com>
2023-05-09 17:24:57 +0700

    Update Bank.java

--- src/bankingapplication3/Bank.java
+++ src/bankingapplication3/Bank.java
@@ -1,7 +1,9 @@
 
 package bankingapplication3;
 
-
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class Bank {
     private String name;
 
@@ -9,22 +11,65 @@ public class Bank {
         this.name = name;
     }
     public void listAccount(){
-    
+        Connection con=BankConnection.connect();
+        try {
+            Statement statement = con.createStatement();
+            String sql="select * from account";
+            ResultSet results=statement.executeQuery(sql);
+            
+            while(results.next()){
+                System.out.println(results.getString(1)+" "+results.getString(2)+" "+results.getString(3));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Bank.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     public void openAccount(Account account){
-        
+        Connection con=BankConnection.connect();
+        String sql="INSERT INTO account(accID,accName,accBalance) values (?,?,?)";
+        try {
+            PreparedStatement preparedStatement=con.prepareStatement(sql);
+            preparedStatement.setInt(1,account.getNumber()); 
+            preparedStatement.setString(2,account.getName());
+            preparedStatement.setDouble(3,account.getBalance()); 
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Bank.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     public void closeAccount(int number){
-    
+        Connection con=BankConnection.connect();
+        String sql="delete from account where accID=?";
+        try {
+            PreparedStatement preparedStatement=con.prepareStatement(sql);
+            preparedStatement.setInt(1, number);
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Bank.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     public void depositMoney(Account account,double amount){
         account.deposit(amount);
+        System.out.println(account.getBalance());
+        Connection con=BankConnection.connect();
+        
+        String sql="UPDATE account set accbalance =? where accID=?";
+        try {
+            PreparedStatement preparedStatement=con.prepareStatement(sql);
+             preparedStatement.setDouble(1, account.getBalance());
+             preparedStatement.setInt(2, account.getNumber());
+             preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Bank.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     public void withdrawMoney(Account account,double amount){
         account.withdraw(amount);
+        
     }
     public Account getAccount(int number){
-        String accountName="gjisjvlksm";
+        String accountName="baitong";
         double balance=500;
         Account account=new Account(number,accountName,balance);
         return account;
